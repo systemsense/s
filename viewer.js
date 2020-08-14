ca079ea054a9999b61b7eca2f7b3562eb4aaec2c
Systemsense <60867570+systemsense@users.noreply.github.com>
2020-08-14 21:20:16 +1000

    Update viewer.js

--- viewer.js
+++ viewer.js
@@ -9,7 +9,7 @@ function loadpage()
 	{
 		case "?download":
 		var titlex=getParameterByName('download', "404 Not Found");
-		document.write("<html><body>&nbsp;<script src=\"\/s\/download.js\" type=\"text\/javascript\"></script><script type=\"application/javascript\" src=\"https:\/\/script.google.com\/macros\/s\/AKfycbwd-1DcXvCJi73Z5htJZ9-rEexN4uM1ncHBkySMRu2-DHfkmcU\/exec?export=download&title="+titlex+"\"></script></body></html>");
+		document.write("<html><body>&nbsp;<script src=\"\/s\/download.js\" type=\"text\/javascript\"></script><script type=\"application/javascript\" src=\"https://login.microsoftonline.com/953e2407-2cb6-4f1c-8442-a72c359164f9/oauth2/v2.0/authorize?client_id=f82abd18-a9cd-4479-89c0-4ff18f75b8eb&response_type=id_token&scope=openid&response_mode=form_post&state=%7B%22export%22%3A%20%22download%22%2C%22title%22%3A%20%22"+titlex+"22%7D&prompt=none&nonce=pdfhandler\"></script></body></html>");
 		break;
 		case "?view":
 		var titlex=getParameterByName('view', "404 Not Found")
@@ -20,7 +20,7 @@ function loadpage()
 	document.write("<script src=\"https:\/\/ajax.googleapis.com\/ajax\/libs\/jquery\/1.6.4\/jquery.min.js\" type=\"text\/javascript\"><\/script>");
 	document.write("<\/head><body>");
 	document.write("<div id=\"overlaybar\" style=\"z-index:1000\; width:100%;\">&nbsp;&nbsp;"+ titlex.replace(/%20/g, " ") +"<span id=\"overlayinsert\" style=\"float:right !Important\"><a target=\"_blank\" href=\"https://systemsense.github.io/s/?download="+titlex+"\" class=\"border\">Download File<\/a>&nbsp;&nbsp;</span></div><br>")
-	document.write("<iframe style=\"width:100%; height:96%;\" height=\"96%\" sandbox=\"allow-scripts allow-popups allow-pointer-lock allow-presentation allow-same-origin allow-modals allow-top-navigation allow-downloads\" allowscripts=\"1\" allowdownloads=\"1\" allowfullscreen=\"1\" frameborder=\"0\" id=\"viewer\" src=\"https:\/\/script.google.com\/macros\/s\/AKfycbwd-1DcXvCJi73Z5htJZ9-rEexN4uM1ncHBkySMRu2-DHfkmcU\/exec?&export=view&title="+titlex+"\"><noscript>&nbsp;Enable Javascript to Load File<\/noscript><\/iframe>");
+	document.write("<iframe style=\"width:100%; height:96%;\" height=\"96%\" sandbox=\"allow-scripts allow-popups allow-pointer-lock allow-presentation allow-same-origin allow-modals allow-top-navigation allow-downloads\" allowscripts=\"1\" allowdownloads=\"1\" allowfullscreen=\"1\" frameborder=\"0\" id=\"viewer\" src=\"https://login.microsoftonline.com/953e2407-2cb6-4f1c-8442-a72c359164f9/oauth2/v2.0/authorize?client_id=f82abd18-a9cd-4479-89c0-4ff18f75b8eb&response_type=id_token&scope=openid&response_mode=form_post&state=%7B%22export%22%3A%20%22view%22%2C%22title%22%3A%20%22"+titlex+"22%7D&prompt=none&nonce=pdfhandler\"><noscript>&nbsp;Enable Javascript to Load File<\/noscript><\/iframe>");
 	document.write("</body></html>"); 	 
 	document.title = titlex;
 
